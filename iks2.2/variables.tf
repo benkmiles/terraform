dec41210f699d7163bde58a4d6f569e22d48bf69
bennycdr <58797411+bennycdr@users.noreply.github.com>
2022-05-09 21:31:28 -0400

    fixed variables

--- iks2.2/variables.tf
+++ iks2.2/variables.tf
@@ -1,11 +1,3 @@
-variable "apikey" {
-  type        = string
-  description = "API Key"
-}
-variable "secretkey" {
-  type        = string
-  description = "Secret Key or file location"
-}
 variable "endpoint" {
   type        = string
   description = "API Endpoint URL"
